80415b22d9cbd4063560a2c2b8d97e956136612b
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-09-08 12:09:23 +0530

    Add Field Update Rows

--- EmployeePayrollServices.sql
+++ EmployeePayrollServices.sql
@@ -12,7 +12,8 @@ Create table employee_payroll
 	Id int identity(1, 1) primary Key,
 	Name varchar(50),
 	Salary money Not Null,
-	Start datetime Not Null
+	Start datetime Not Null,
+	gender VARCHAR(1)
 	)
 
 --UC3 Ability to Create employee_payroll data
@@ -36,3 +37,9 @@ select * from employee_payroll
 
 select * from employee_payroll 
 	WHERE start BETWEEN CAST('2021-01-01' AS DATE) AND CAST('2021-09-01' AS DATE) 
+
+--UC6
+ALTER TABLE employee_payroll ADD gender VARCHAR(1)
+update employee_payroll set gender = 'F' WHere Name = 'Terisha'
+update employee_payroll set gender = 'M' WHere Name = 'Charlie' or Name = 'Bill' 
+update employee_payroll set Salary = 300000 WHere Name = 'Terisha'
